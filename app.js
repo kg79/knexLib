45340f225df7d14eb8d15238a5eb1070d99697db
Kevin Galvan <klgalvan79@gmail.com>
2018-04-10 15:51:00 -0500

    for glory!

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3334;
 const knex = require('./knex');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   knex('authors').select('*')
   .then( (bookWriters) => {
-    console.log(bookWriters);
-    res.end('alrighty then')
+    console.log(bookWriters[0].id)
+    res.json(bookWriters[0].id)
   })
 });
 
